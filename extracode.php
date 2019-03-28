0d3c8ac3e30a5b819fb352ea196c177380d1dba0
Test <46293565+vijayrathod245@users.noreply.github.com>
2019-03-28 14:28:50 +0530

    Add files via upload

---
+++ extracode.php
@@ -0,0 +1,42 @@
+<?php
+/* Select particular name */
+
+$q = $this->db->query('SELECT age FROM my_users_table WHERE id = ?',array(3));
+$data = array_shift($q->result_array());
+echo($data['age']);
+
+Or
+
+$this->db->select('age');
+$this->db->where('id', '3');
+$q = $this->db->get('my_users_table');
+// if id is unique, we want to return just one row
+$data = array_shift($q->result_array());
+
+echo($data['age']);
+
+Or
+
+// here we select just the age column
+$this->db->select('age');
+$this->db->where('id', '3');
+$q = $this->db->get('my_users_table');
+$data = $q->result_array();
+
+echo($data[0]['age']);
+
+Or
+
+$this->db->where('id', '3');
+// here we select every column of the table
+$q = $this->db->get('my_users_table');
+$data = $q->result_array();
+
+echo($data[0]['age']);
+
+
+/* Last to second record show */
+
+SELECT countryname FROM `country` GROUP BY countryname DESC LIMIT 1 OFFSET 1
+
+
